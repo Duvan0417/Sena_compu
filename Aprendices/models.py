b59133850d2a27b741a3fadfafa3b8fb9df8fdbe
Duvan0417 <narvaezduvan29@gmail.com>
2025-11-28 08:57:24 -0500

    correcion de atributos de los modelos

--- Aprendices/models.py
+++ Aprendices/models.py
@@ -15,3 +15,7 @@ class Uso_computador(models.Model):
     ID_aprendiz = models.ForeignKey(Aprendiz, on_delete=models.CASCADE)
     ID_computador = models.ForeignKey(Computador, on_delete=models.CASCADE)
     ID_User = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_inicio = models.DateField(null=True, blank=True)
+    fecha_final = models.DateField(null=True, blank=True)
+    hora_inicio = models.TimeField(null=True, blank=True)    
+    hora_final = models.TimeField(null=True, blank=True)
